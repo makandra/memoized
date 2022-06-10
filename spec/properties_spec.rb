f04d07b8149cced9469116192bae7eee56538c8d
Klaus Weidinger <klaus.weidinger@makandra.de>
2022-06-10 14:09:16 +0200

    Complete prop_check implementation

--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -4,23 +4,118 @@
 #
 # end
 
+class MemoizedParameters
+  attr_accessor :req_params, :opt_params, :rest_params, :keyreq_params, :key_params, :keyrest_params
+
+  def initialize(parameters = [])
+    # This constructor does not check, whether the parameters were ordered correctly
+    # with respect to the Ruby language specification. However, all outputs will be sorted correctly.
+    clear_params
+    parameters.each do |(param_type, param_name)|
+      case param_type
+      when :req
+        @req_params << [param_type, param_name]
+      when :opt
+        @opt_params << [param_type, param_name]
+      when :rest
+        @rest_params << [param_type, param_name]
+      when :keyreq
+        @keyreq_params << [param_type, param_name]
+      when :key
+        @key_params << [param_type, param_name]
+      when :keyrest
+        @keyrest_params << [param_type, param_name]
+      else raise "unknown parameter type"
+      end
+    end
+  end
+
+  def clear_params
+    @req_params = []
+    @opt_params = []
+    @rest_params = []
+    @keyreq_params = []
+    @key_params = []
+    @keyrest_params = []
+  end
+
+  def params
+    @req_params + @opt_params + @rest_params + @keyreq_params + @key_params + @keyrest_params
+  end
+
+  def signature
+    params.map(&MemoizedParameters.method(:to_signature)).join(', ')
+  end
+
+  def self.to_signature((param_type, param_name))
+    case param_type
+    when :req
+      "#{param_name}"
+    when :opt
+      "#{param_name} = 42"
+    when :rest
+      "*#{param_name}"
+    when :keyreq
+      "#{param_name}:"
+    when :key
+      "#{param_name}: 42"
+    when :keyrest
+      "**#{param_name}"
+    else raise "unknown parameter type"
+    end
+  end
+
+  def cache_key
+    params.map(&MemoizedParameters.method(:to_cache_key)).join("\n")
+  end
+
+  def self.to_cache_key((param_type, param_name))
+    case param_type
+    when :req
+      "all_args.push(#{param_name})"
+    when :opt
+      "all_args.push(#{param_name}) unless #{param_name}.equal?(42)"
+    when :rest
+      "all_args.push(*#{param_name})"
+    when :keyreq
+      "all_kwargs[:#{param_name}] = #{param_name}"
+    when :key
+      "all_kwargs[:#{param_name}] = #{param_name} unless #{param_name}.equal?(42)"
+    when :keyrest
+      "all_kwargs.merge!(#{param_name})"
+    else raise "unknown parameter type"
+    end
+  end
+
+end
+
 
 describe "#memoize" do
   include PropCheck
   include PropCheck::Generators
-  # include PropCheck::Generators::Set
   include Memoized
 
   it "does not change the method's parameters" do
-    forall(req: array(simple_symbol)) do |req:|
-
-      req_params = req.map(&:to_s).uniq.map { |r| "req_#{r}"}.join(', ')
+    forall(
+      array(
+        tuple(
+          one_of(
+            constant(:req), constant(:opt), constant(:rest), constant(:keyreq), constant(:key), constant(:keyrest)
+          ),
+          simple_symbol.map do |sym|
+            "param_#{sym}".to_sym
+          end
+        )
+      )
+    ) do |parameters|
+      # params now have proper names (no :"", no Ruby keywords) due to the .map in the generator above
+      mp = MemoizedParameters.new(parameters.uniq(&:second))
 
       eval(<<-RUBY)
         class MemoizedPropertyClass
           include Memoized
 
-          def parameter_dummy(#{req_params})
+          def parameter_dummy(#{mp.signature})
             42
           end
 
@@ -30,9 +125,8 @@ describe "#memoize" do
         end
       RUBY
 
-
-
-      expect(MemoizedPropertyClass.instance_variable_get(:@new_parameters)).to eq(MemoizedPropertyClass.instance_variable_get(:@old_parameters))
+      expect(MemoizedPropertyClass.instance_variable_get(:@new_parameters))
+        .to eq(MemoizedPropertyClass.instance_variable_get(:@old_parameters))
     end
   end
 end
